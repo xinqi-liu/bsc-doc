7385dc755642f3ce5735704b28fb0e197fa06e8f
xinqi-liu <blank@live.cn>
2016-11-26 00:56:16 +0800

    Create ApiAlertUtilsApiIdModel.java

---
+++ monitor/monitorUtils/ApiAlertUtilsApiIdModel.java
@@ -0,0 +1,9 @@
+package monitorUtils;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+public class ApiAlertUtilsApiIdModel {
+    public Float pvCountTotal;
+    public Float sumElapse;
+    public Map<String,Float> httpCodeMap;
+}
