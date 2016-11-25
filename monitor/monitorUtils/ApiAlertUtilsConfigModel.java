97dffb354bd833e592ceb4635f415483292a1095
xinqi-liu <blank@live.cn>
2016-11-26 00:57:06 +0800

    Create ApiAlertUtilsConfigModel.java

---
+++ monitor/monitorUtils/ApiAlertUtilsConfigModel.java
@@ -0,0 +1,10 @@
+package monitorUtils;
+import java.util.ArrayList;
+import java.util.Map;
+public class ApiAlertUtilsConfigModel {
+    public String period;
+    public String apiId;
+    public String indicatorId;
+    public String strategyId;
+    public ArrayList<Map<String,String>> content; 
+}
